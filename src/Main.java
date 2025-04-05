bb100e0a01719f4a436d08d7f7ef8fd8ce04a5d9
SAWA-SAWA-Joseph <harrietlolando@gmail.com>
2025-04-05 12:21:20 +0300

    commit#3
    This class consumes the DataService interface and is agnostic of the specific implementation. It will use constructor injection to receive the DataService

--- src/Main.java
+++ src/Main.java
@@ -31,3 +31,18 @@ public class FileDataService implements DataService {
         return "Data read from file located at: " + filePath;
     }
 }
+// Step 3: Service consumer class
+public class DataProcessor {
+    private DataService dataService;
+
+    // Constructor injection of the DataService
+    public DataProcessor(DataService dataService) {
+        this.dataService = dataService;
+    }
+
+    public void process() {
+        // Delegate the task to the injected service
+        String result = dataService.performTask();
+        System.out.println("Processed result: " + result);
+    }
+}
